a4082387df63e73a09e140e0d9d911683f4c543d
ChrisGeo4521 <ch.georganas@gmail.com>
2023-03-09 17:50:04 +0200

    Basic navbar structure completed

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,17 +2,22 @@
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
 </script>
 
-<Navbar let:hidden let:toggle rounded color="form">
+<Navbar let:hidden let:toggle class="bg-[#75202e]">
   <NavBrand href="/">
-    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    <img
+      src="./img/LC_Xanthi_white.png"
+      class="object-contain h-24 h-24 text-white"
+      alt="LC Xanthi Logo"
+    />
+    <span class="self-center whitespace-nowrap text-xl font-semibold text-white text-[25px]">
+      Jobfair
+    </span>
   </NavBrand>
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
-    <NavLi href="/" active={true}>Home</NavLi>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/services">Services</NavLi>
-    <NavLi href="/pricing">Pricing</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
+    <NavLi class="text-[#ffffff] text-[20px]" href="/">Home</NavLi>
+    <NavLi class="text-[#ffffff] text-[20px]" href="/about">About</NavLi>
+    <NavLi class="text-[#ffffff] text-[20px]" href="/sponsors">Sponsors</NavLi>
+    <NavLi class="text-[#ffffff] text-[20px]" href="/contact">Contact</NavLi>
   </NavUl>
 </Navbar>
