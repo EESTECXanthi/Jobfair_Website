a6aaae789db188593a2ff3d9824ca3a7ac0627a8
ChrisGeo4521 <ch.georganas@gmail.com>
2023-03-09 21:42:02 +0200

    Added a Jobfair color pallete. Added Jobfair logos to assets.

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -9,15 +9,12 @@
       class="object-contain h-24 h-24 text-white"
       alt="LC Xanthi Logo"
     />
-    <span class="self-center whitespace-nowrap text-xl font-semibold text-white text-[25px]">
-      Jobfair
-    </span>
   </NavBrand>
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
-    <NavLi class="text-[#ffffff] text-[20px]" href="/">Home</NavLi>
-    <NavLi class="text-[#ffffff] text-[20px]" href="/about">About</NavLi>
-    <NavLi class="text-[#ffffff] text-[20px]" href="/sponsors">Sponsors</NavLi>
-    <NavLi class="text-[#ffffff] text-[20px]" href="/contact">Contact</NavLi>
+    <NavLi class="text-white text-[20px]" href="/">Home</NavLi>
+    <NavLi class="text-white text-[20px]" href="/about">About</NavLi>
+    <NavLi class="text-white text-[20px]" href="/sponsors">Sponsors</NavLi>
+    <NavLi class="text-white text-[20px]" href="/contact">Contact</NavLi>
   </NavUl>
 </Navbar>
