f0f2f439ec9768c38cc3ca2023e7c40005400364
ChrisGeo4521 <ch.georganas@gmail.com>
2023-03-15 21:44:39 +0200

    Combined logos for better placement.

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,13 +2,15 @@
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
 </script>
 
-<Navbar let:hidden let:toggle class="bg-[#75202e]">
+<Navbar let:hidden let:toggle class="bg-[#76202e]">
   <NavBrand href="/">
+    <span class="w-5" />
     <img
-      src="./img/LC_Xanthi_white.png"
-      class="object-contain h-24 h-24 text-white"
-      alt="LC Xanthi Logo"
+      src="./img/combined_logo.png"
+      class="object-contain h-24 w-64 text-white"
+      alt="LC Xanthi Logo + Jobfair Logo"
     />
+    <span class="w-5" />
   </NavBrand>
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
