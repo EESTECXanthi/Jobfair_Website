e7e79290b004b89d360932709e1e7f3ab4324d95
ChrisGeo4521 <ch.georganas@gmail.com>
2023-03-22 16:37:54 +0200

    Deleted everything svelte-related. Reconstructed project with pure HTML.

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Navbar from "./lib/Navbar.svelte";
-  import Home from "./lib/Home.svelte";
-  import AboutUs from "./lib/about_us.svelte";
-</script>
-
-<Navbar />
-<Home />
-<AboutUs />
